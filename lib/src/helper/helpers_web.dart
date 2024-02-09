de04b8b9cec683a61b47353199c14cec43f9943b
Joscha <34318751+josxha@users.noreply.github.com>
2024-02-09 02:32:05 +0100

    fix missing `sqlite3.open` on web

--- lib/src/helper/helpers_web.dart
+++ lib/src/helper/helpers_web.dart
@@ -1,5 +1,7 @@
 import 'dart:typed_data';
 
+import 'package:sqlite3/common.dart';
+
 // do nothing
 
 void loadSqLiteLib(String? sqlitePath) {}
@@ -8,4 +10,15 @@ extension U8intListExtension on Uint8List {
   Uint8List gzipEncode() => Uint8List(0);
 
   Uint8List gzipDecode() => Uint8List(0);
+}
+
+/// sqlite3 is not defined in the wasm sqlite3 library
+const sqlite3 = SqLite3();
+
+class SqLite3 {
+  const SqLite3();
+
+  CommonDatabase open(String mbtilesPath, {required OpenMode mode}) {
+    throw UnimplementedError();
+  }
 }
