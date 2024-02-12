abd36d17f3baa4496857dc843fa3ca87f7a48ddc
Joscha <34318751+josxha@users.noreply.github.com>
2024-02-12 02:48:21 +0100

    Update mbtiles_test.dart

--- test/mbtiles_test.dart
+++ test/mbtiles_test.dart
@@ -8,26 +8,27 @@ import 'utils.dart';
 
 void main() {
   test('Open read only raster Database', () {
-    final _ = MbTiles(
+    final mbtiles = MbTiles(
       mbtilesPath: rasterMbtilesPath,
       sqlitePath: sqliteLibPath,
     );
+    mbtiles.dispose();
   });
   test('Open read write raster Database', () {
     final mbtiles = MbTiles(
       mbtilesPath: rasterMbtilesPath,
       sqlitePath: sqliteLibPath,
-      editable: true,
     );
     expect(mbtiles.getTile(z: 0, x: 0, y: 0), isA<Uint8List>());
     expect(mbtiles.getTile(z: 20, x: 123, y: 456), isNull);
+    mbtiles.dispose();
   });
   test('Create Database', () {
     final file = File('tmp.mbtiles');
     final mbTiles = MbTiles.create(
       mbtilesPath: file.path,
       sqlitePath: sqliteLibPath,
-      metadata: const MBTilesMetadata(name: 'TestFiel', format: 'pbf'),
+      metadata: const MBTilesMetadata(name: 'TestFile', format: 'pbf'),
     );
     mbTiles.putTile(z: 0, x: 0, y: 0, bytes: Uint8List(0));
     mbTiles.dispose();
